4158a0f86d88191c677c61727f6a967f071ce8b6
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-02-27 22:31:25 +0000

    Auto-generated commit [ci skip]

--- src/app/recipe-book/recipe-list/recipe-item/recipe-item.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'app-recipeItem',
-    templateUrl: './recipe-item.component.html',
-    styleUrls: ['./recipe-item.component.css']
-})
-export class RecipeItemComponent {
-}
--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-    selector: 'app-shoppingEdit',
-    templateUrl: './shopping-edit.component.html',
-    styleUrls: ['./shopping-edit.component.css']
-})
-export class ShoppingEditComponent {
-}
