2c7930b5799206fc746c524a8fcbc98e5f146f7a
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-02-27 21:59:29 +0000

    display the recipe item element

--- src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,10 +1,15 @@
 import { Component } from "@angular/core";
 
+import { Recipe } from "../recipe.model";
+
 @Component({
     selector: 'app-recipeList',
     templateUrl: './recipe-list.component.html',
     styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-    recipes = [];
+    recipes: Recipe[] = [
+        new Recipe('A Test Recipe', 'This is simply a test.', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_1280.jpg'),
+        new Recipe('A Test Recipe', 'This is simply a test.', 'https://cdn.pixabay.com/photo/2020/02/02/15/07/meat-4813261_1280.jpg'),
+    ];
 }
